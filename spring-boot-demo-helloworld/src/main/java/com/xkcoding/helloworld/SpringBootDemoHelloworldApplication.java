313268b4ce23729e51f999ac5bc273f9b77df10b
yuangong <yuangong@tuhuadmindeMacBook-Air.local>
2020-11-18 16:29:31 +0800

    init

--- spring-boot-demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplication.java
+++ spring-boot-demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplication.java
@@ -1,11 +1,18 @@
 package com.xkcoding.helloworld;
 
-import cn.hutool.core.util.StrUtil;
+import com.tuhu.renault.autoconfigure.provider.CacheManager;
+import com.tuhu.renault.common.exception.CacheParamException;
+import com.tuhu.renault.common.exception.TooLongKeyOrValueException;
+import com.tuhu.renault.chassis.tikv.ChassisClient;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * <p>
@@ -24,21 +31,62 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringBootDemoHelloworldApplication {
 
+    private static ChassisClient<String, Object> cache;
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoHelloworldApplication.class, args);
+        System.setProperty("java._appid_", "int-website-arch-renault-portal-api");
+        System.setProperty("java._environment_", "work");
+        System.setProperty("apollo.meta", "http://apollo.tuhu.work:8090");
+        System.setProperty("env", "DEV");
+
+	    SpringApplication.run(SpringBootDemoHelloworldApplication.class, args);
+
+        cache = (ChassisClient) CacheManager.getCache("testtikv");
 	}
 
-	/**
-	 * Hello，World
-	 *
-	 * @param who 参数，非必须
-	 * @return Hello, ${who}
-	 */
 	@GetMapping("/hello")
 	public String sayHello(@RequestParam(required = false, name = "who") String who) {
-		if (StrUtil.isBlank(who)) {
-			who = "World";
-		}
-		return StrUtil.format("Hello, {}!", who);
+//		if (StrUtil.isBlank(who)) {
+//			who = "World";
+//		}
+//		return StrUtil.format("Hello, {}!", who);
+
+        tikvTest();
+        return "";
 	}
+
+    public static void tikvTest() {
+        try {
+
+            Random r = new Random();
+            int i = r.nextInt();
+            String key = "key" + i;
+            String value = "value" + i;
+            cache.set(key, value);
+
+            Object value2 = cache.get(key);
+            if (value2 != null && value2.toString().equals(value)) {
+                System.out.println("SET and GET success!");
+            } else {
+                System.out.println("SET or GET failed!");
+            }
+
+//            cache.delete(key);
+//
+//            value2 = cache.get(key);
+//            Assert.isTrue((value2 == null || value2.toString().isEmpty()), () -> {
+//                System.out.println("Delete failed!!");
+//                return "Delete failed!";
+//            });
+
+        } catch (TooLongKeyOrValueException e) {
+            e.printStackTrace();
+        } catch (CacheParamException e) {
+            e.printStackTrace();
+        }
+
+        // For non spring environment, please call closeCache to release connection
+        CacheManager.closeCache(cache);
+    }
+
 }
